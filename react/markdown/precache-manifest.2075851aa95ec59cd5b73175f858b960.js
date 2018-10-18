bde1d550c87b27749f635a8b7a121d115afaf174
Renato Francia <rmfranciacastillo@gmail.com>
2018-10-18 11:26:31 -0500

    Added: Markdown Project

---
+++ react/markdown/precache-manifest.2075851aa95ec59cd5b73175f858b960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "eebf55d7d605ffa07f0e",
+    "url": "./static/js/main.eebf55d7.chunk.js"
+  },
+  {
+    "revision": "e372fb6b849b16ad25e0",
+    "url": "./static/js/1.e372fb6b.chunk.js"
+  },
+  {
+    "revision": "eebf55d7d605ffa07f0e",
+    "url": "./static/css/main.e53ee303.chunk.css"
+  },
+  {
+    "revision": "12984ec9e4fbe9106ae215c544e6e711",
+    "url": "./index.html"
+  }
+];
