9be156114aa9d55b4b7442d589e3e939280708f5
Renato Francia <rmfranciacastillo@gmail.com>
2018-10-19 20:06:55 -0500

    Added: Drum Machine Project

---
+++ react/drum-machine/precache-manifest.605d12957636f4a1619f45cc84cc48f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c5c214d9e77d66d3adf9",
+    "url": "./static/js/main.c5c214d9.chunk.js"
+  },
+  {
+    "revision": "3983a47db0adc3e20a55",
+    "url": "./static/js/1.3983a47d.chunk.js"
+  },
+  {
+    "revision": "c5c214d9e77d66d3adf9",
+    "url": "./static/css/main.662eca06.chunk.css"
+  },
+  {
+    "revision": "4a03bef70809690015b8715d4f2fd9bb",
+    "url": "./index.html"
+  }
+];
