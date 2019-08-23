2c8dd2053f13b0273f8578299a4260abefa22e49
Renato Francia <rmfranciacastillo@gmail.com>
2019-08-23 16:21:33 -0500

    Updated: Social network route

---
+++ webapps/react/social-network/precache-manifest.13317eae8ff5a5dd5de7dc20f3f8e000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "./static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6d8e1f083199b0d9dc2f",
+    "url": "./static/js/main.5c3e60d3.chunk.js"
+  },
+  {
+    "revision": "d84bbc00594edba7f2bb",
+    "url": "./static/js/2.1fdac75c.chunk.js"
+  },
+  {
+    "revision": "6d8e1f083199b0d9dc2f",
+    "url": "./static/css/main.89dbbc58.chunk.css"
+  },
+  {
+    "revision": "3141b5212b178de3a806975cdea06a42",
+    "url": "./index.html"
+  }
+];
