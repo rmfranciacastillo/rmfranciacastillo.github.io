348e62c7587ba68929afc81e0c66fdc53d6d9b9b
Renato Francia <rmfranciacastillo@gmail.com>
2019-08-23 16:10:20 -0500

    Added: Social media Webapp

---
+++ webapps/react/social-network/precache-manifest.c0df46dcc58bc71c276748ada89562f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67350f3bfcb2360ccb24",
+    "url": "/static/js/main.719bf4bc.chunk.js"
+  },
+  {
+    "revision": "d84bbc00594edba7f2bb",
+    "url": "/static/js/2.1fdac75c.chunk.js"
+  },
+  {
+    "revision": "67350f3bfcb2360ccb24",
+    "url": "/static/css/main.89dbbc58.chunk.css"
+  },
+  {
+    "revision": "00bad2cc5bbd684494e0248a0d512c7b",
+    "url": "/index.html"
+  }
+];
